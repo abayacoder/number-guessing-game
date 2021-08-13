6ffc666c9043feaf1fd623d31463fdfca9c3722b
abayacoder <73934658+abayacoder@users.noreply.github.com>
2021-08-13 18:03:30 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,60 @@
+introString = input ("enter your introduction:")
+print(introString)
+
+characterCount=0
+wordCount=1
+
+Number  guessing game 
+Guess the number (between 1 and 19);
+
+
+
+Enter your guess :2
+your guess was too low : Guess a number higher than 2
+
+
+
+Enter your guess :3
+your guess was too low : Guess a number higher than 3
+
+
+
+
+Enter your guess :4
+your guess was too low : Guess a number higher than 4
+
+
+
+Enter your guess :5
+your guess was too low : Guess a number higher than 5
+
+
+
+Enter your guess :6
+your guess was too low : Guess a number higher than 6
+
+
+
+Enter your guess :7
+your guess was too low : Guess a number higher than 7
+
+
+
+Enter your guess :8
+your guess was too low : Guess a number higher than 8
+
+
+
+Enter your guess :9
+your guess was too low : Guess a number higher than 9
+
+Congratulation You Won!!!
+
+while chance < 5:
+
+if guess == number:
+   print ("Congratulation You Won!!!")
+   break
+
+ if not chance < 5:
+     print ("YOU LOSE !!! The number is ",number)  
